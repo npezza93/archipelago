19fe0806f6e6c5c16ff45d791f171c94765c8fd6
npezza93 <npezza93@gmail.com>
2017-08-07 21:09:27 -0400

    Double quote styling touch ups

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
-const {app, BrowserWindow} = require('electron');
-const path = require('path');
-const url  = require('url');
+const {app, BrowserWindow} = require("electron")
+const path                 = require("path")
+const url                  = require("url")
 
 let win
 
@@ -9,20 +9,19 @@ function createWindow () {
     width: 1000,
     height: 600,
     titleBarStyle: "hidden",
-    vibrancy: 'ultra-dark'
+    vibrancy: "ultra-dark"
   })
 
   win.loadURL(url.format({
-    pathname: path.join(__dirname, '/app/index.html'),
-    protocol: 'file:',
+    pathname: path.join(__dirname, "/app/index.html"),
+    protocol: "file:",
     slashes: true
-  }));
+  }))
 
-  win.focus();
-  win.webContents.openDevTools();
+  win.focus()
 
   // Emitted when the window is closed.
-  win.on('closed', () => {
+  win.on("closed", () => {
     // Dereference the window object, usually you would store windows
     // in an array if your app supports multi windows, this is the time
     // when you should delete the corresponding element.
@@ -30,17 +29,17 @@ function createWindow () {
   })
 }
 
-app.on('ready', createWindow)
+app.on("ready", createWindow)
 
-app.on('window-all-closed', () => {
+app.on("window-all-closed", () => {
   // On macOS it is common for applications and their menu bar
   // to stay active until the user quits explicitly with Cmd + Q
-  if (process.platform !== 'darwin') {
+  if (process.platform !== "darwin") {
     app.quit()
   }
 })
 
-app.on('activate', () => {
+app.on("activate", () => {
   // On macOS it's common to re-create a window in the app when the
   // dock icon is clicked and there are no other windows open.
   if (win === null) {
