ddf5bdf017d88fb6b41b44b1f6d38995d18845ee
npezza93 <npezza93@gmail.com>
2017-08-07 21:10:05 -0400

    Add double quote styling touch ups. Add resizing

--- app/js/index.js
+++ app/js/index.js
@@ -1,37 +1,48 @@
 const {ipcRenderer} = require("electron")
 const Terminal      = require("xterm")
-const pty           = require('node-pty')
-const defaultShell  = require('default-shell')
+const Pty           = require("node-pty")
+const defaultShell  = require("default-shell")
 
-let log = ''
-let ptyProcess = pty.spawn(defaultShell, [], {
-  name: 'xterm-256color',
+let log = ""
+
+var initRows = Math.floor((window.innerHeight - 25) / 18)
+var initCols = Math.floor((window.innerWidth - 30) / 9)
+
+let pty = Pty.spawn(defaultShell, [], {
+  name: "xterm-256color",
   cwd: process.env.PWD,
-  env: process.env
+  env: process.env,
+  rows: initRows,
+  cols: initCols
 })
+
 let term = new Terminal({
   cursorBlink: true,
   // block | underline | bar
   cursorStyle: "block",
-  rows: Math.floor((window.innerHeight - 25) / 18),
-  cols: Math.floor(window.innerWidth - 15 / 15)
+  rows: initRows,
+  cols: initCols
 })
 
 term.open(document.getElementById("terminal"), true)
 term.element.classList["add"]("fullscreen")
+
 term.on("data", (data) => {
-  ptyProcess.write(data);
+  pty.write(data)
 })
-ptyProcess.on("data", (data) => {
-  term.write(data);
-  log += data;
-})
-term._initialized = true
-
-
-ipcRenderer.on("resize", (event, params) => {
-  term.resize(params.cols, params.rows);
+pty.on("data", (data) => {
+  term.write(data)
+  log += data
 })
 
-document.documentElement.style.setProperty(`--cursor-color`, 'rgba(171, 178, 191, 0.8)')
-document.documentElement.style.setProperty(`--background-color`, 'rgba(40, 44, 52, 0.1)')
+window.addEventListener("resize", function() {
+  terminalElement = document.getElementsByClassName("terminal")[0];
+  rows = Math.floor(terminalElement.offsetHeight / 18);
+  cols = Math.floor(terminalElement.offsetWidth / 9);
+  
+  term.resize(cols, rows);
+  pty.resize(cols, rows);
+})
+
+document.documentElement.style.setProperty("--cursor-color", "rgba(171, 178, 191, 0.8)")
+document.documentElement.style.setProperty("--background-color", "rgba(40, 44, 52, 0.1)")
