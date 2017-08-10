c9f22d39a87116d08e95e748f1220cf6de5cbfde
npezza93 <npezza93@gmail.com>
2017-08-09 22:53:46 -0400

    Extract logic out to classes and add keymaster to handle keyboard shortcuts

--- app/js/index.js
+++ app/js/index.js
@@ -1,48 +1,29 @@
-const {ipcRenderer} = require("electron")
-const Terminal      = require("xterm")
-const Pty           = require("node-pty")
-const defaultShell  = require("default-shell")
+'use strict';
 
-let log = ""
+const key           = require("keymaster");
+const Terminal      = require(__dirname + "/js/terminal");
 
-var initRows = Math.floor((window.innerHeight - 25) / 18)
-var initCols = Math.floor((window.innerWidth - 30) / 9)
+let terminals = {};
 
-let pty = Pty.spawn(defaultShell, [], {
-  name: "xterm-256color",
-  cwd: process.env.PWD,
-  env: process.env,
-  rows: initRows,
-  cols: initCols
-})
+window.addEventListener('resize', function() {
+  var terminalElement = document.getElementsByClassName('terminal')[0];
+  var rows = Math.floor(terminalElement.offsetHeight / 18);
+  var cols = Math.floor(terminalElement.offsetWidth / 9);
+  var pids = Object.keys(terminals);
 
-let term = new Terminal({
-  cursorBlink: true,
-  // block | underline | bar
-  cursorStyle: "block",
-  rows: initRows,
-  cols: initCols
-})
+  for(pid of pids) {
+    terminals[pid].resize(cols, rows);
+  }
+});
 
-term.open(document.getElementById("terminal"), true)
-term.element.classList["add"]("fullscreen")
+function spawnTerminal() {
+  var initTerm = new Terminal();
+  terminals[initTerm.pty.pid] = initTerm;
+  initTerm.open();
+}
 
-term.on("data", (data) => {
-  pty.write(data)
-})
-pty.on("data", (data) => {
-  term.write(data)
-  log += data
-})
+spawnTerminal();
+document.documentElement.style.setProperty("--cursor-color", "rgba(171, 178, 191, 0.8)");
+document.documentElement.style.setProperty("--background-color", "rgba(40, 44, 52, 0.1)");
 
-window.addEventListener("resize", function() {
-  terminalElement = document.getElementsByClassName("terminal")[0];
-  rows = Math.floor(terminalElement.offsetHeight / 18);
-  cols = Math.floor(terminalElement.offsetWidth / 9);
-  
-  term.resize(cols, rows);
-  pty.resize(cols, rows);
-})
-
-document.documentElement.style.setProperty("--cursor-color", "rgba(171, 178, 191, 0.8)")
-document.documentElement.style.setProperty("--background-color", "rgba(40, 44, 52, 0.1)")
+key('⌘+t', spawnTerminal);
