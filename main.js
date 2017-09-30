5eeaa1184d2e354bfd0400d75142be7b6b5f4a73
npezza93 <npezza93@gmail.com>
2017-09-30 10:25:35 -0400

    Cleanup inline theme field styles

--- main.js
+++ main.js
@@ -128,7 +128,6 @@ function createWindow () {
 
   win.focus()
 
-  // Emitted when the window is closed.
   win.on('closed', () => {
     // Dereference the window object, usually you would store windows
     // in an array if your app supports multi windows, this is the time
@@ -144,16 +143,12 @@ function createWindow () {
 app.on('ready', createWindow)
 
 app.on('window-all-closed', () => {
-  // On macOS it is common for applications and their menu bar
-  // to stay active until the user quits explicitly with Cmd + Q
   if (process.platform !== 'darwin') {
     app.quit()
   }
 })
 
 app.on('activate', () => {
-  // On macOS it's common to re-create a window in the app when the
-  // dock icon is clicked and there are no other windows open.
   if (win === null) {
     createWindow()
   }
