5e2bc6fc5c56df522f1a11fe38cded0f909431b5
npezza93 <npezza93@gmail.com>
2018-01-13 14:59:37 -0500

    Add keymap manager to global scope

--- app/renderer/init.js
+++ app/renderer/init.js
@@ -1,6 +1,18 @@
 require('coffeescript').register()
-ConfigFile   = require('../config_file')
+KeymapManager = require('atom-keymap')
+Config   = require('../config')
 
-global.archipelago = { config: new ConfigFile() }
+global.archipelago = { config: new Config, keymaps: new KeymapManager }
+
+archipelago.keymaps.mappings = Object.values(
+  archipelago.config.get('keybindings')[process.platform]
+).map((keybinding) => {
+  return {
+    'keystroke': keybinding.accelerator,
+    'command': keybinding.command.map((num) => {
+      return String.fromCharCode(parseInt(num))
+    }).join('')
+  }
+})
 
 require('./renderer')
