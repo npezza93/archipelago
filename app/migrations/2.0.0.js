ba76ee80394d27e3c9e09443147f42c3a93f0e7a
Nick Pezza <npezza93@gmail.com>
2018-08-25 21:13:54 -0400

    Decaffeinate migrations :coffee:

---
+++ app/migrations/2.0.0.js
@@ -0,0 +1,16 @@
+module.exports = {
+  run(config) {
+    const { contents } = config;
+
+    if (contents.activeProfileId === undefined) {
+      contents.activeProfileId = contents.activeProfile;
+      delete contents.activeProfile;
+    }
+
+    for (let profileId of Object.keys(contents.profiles)) {
+      delete contents.profiles[profileId].keybindings;
+    }
+
+    return config._writeSync(contents);
+  }
+};
