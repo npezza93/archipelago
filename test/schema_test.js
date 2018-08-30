6b20d73764f2a7c0d4d21c2a738633a29072f58d
Nick Pezza <npezza93@gmail.com>
2018-08-30 19:21:28 -0400

    Test getDefaultValue

--- test/schema_test.js
+++ test/schema_test.js
@@ -1,17 +1,18 @@
-const { assert } = require('chai');
-const Schema = require('../app/schema');
+const { assert }  = require('chai');
+const Schema      = require('../app/schema');
+const oldPlatform = process.platform;
 
 describe('Schema', () => {
-  const oldPlatform = process.platform;
-  before(() => {
-    Object.defineProperty(process, 'platform', { value: 'darwin' });
-  });
-
-  after(() => {
-    Object.defineProperty(process, 'platform', { value: oldPlatform });
-  });
-
   describe('property', () => {
+    before(() => {
+      Object.defineProperty(process, 'platform', { value: 'darwin' });
+    });
+
+    after(() => {
+      console.log(oldPlatform);
+      Object.defineProperty(process, 'platform', { value: oldPlatform });
+    });
+
     it('is enabled on the specified platform', () => {
       let schema = new Schema;
       let property = {
@@ -53,6 +54,15 @@ describe('Schema', () => {
   });
 
   describe('getSchema', () => {
+    before(() => {
+      Object.defineProperty(process, 'platform', { value: 'darwin' });
+    });
+
+    after(() => {
+      console.log(oldPlatform);
+      Object.defineProperty(process, 'platform', { value: oldPlatform });
+    });
+
     it('traverses the schema to get nested property schemas', () => {
       let schema = new Schema;
 
@@ -95,5 +105,52 @@ describe('Schema', () => {
   });
 
   describe('getDefaultValue', () => {
+    before(() => {
+      Object.defineProperty(process, 'platform', { value: 'darwin' });
+    });
+
+    after(() => {
+      Object.defineProperty(process, 'platform', { value: oldPlatform });
+    });
+
+    it('is undefined when there is no defined defaultValue', () => {
+      let schema = new Schema;
+
+      assert.isUndefined(schema.defaultValue('shell'));
+    });
+
+    it('fetches the default value for normal types', () => {
+      let schema = new Schema;
+
+      assert.equal(schema.defaultValue('shellArgs'), '--login');
+    });
+
+    it('fetches the default value all properties in an object type', () => {
+      let schema = new Schema;
+      let defaultTheme = {
+        selection: 'rgba(255, 255, 255, 0.3)', foreground: '#FFFFFF',
+        background: '#000000', black: '#000000', red: '#ff5c57',
+        brightRed: '#ff5c57', green: '#5af78e', brightGreen: '#5af78e',
+        yellow: '#f3f99d', brightYellow: '#f3f99d', magenta: '#ff6ac1',
+        brightMagenta: '#ff6ac1', cyan: '#5fcbd8', brightCyan: '#5fcbd8',
+        blue: '#57c7ff', brightBlue: '#57c7ff', white: '#ffffff',
+        brightWhite: '#FFFFFF', brightBlack: '#808080', cursorAccent: '#000000',
+        cursor: '#FFFFFF'
+      }
+      assert.deepEqual(schema.defaultValue('theme'), defaultTheme);
+    });
+
+    it('fetches the default value for platform specific properties', () => {
+      let schema = new Schema;
+      let defaultKeybindings = [
+        { keystroke: 'cmd-left', command: '\\x1bOH' },
+        { keystroke: 'cmd-right', command: '\\x1bOF' },
+        { keystroke: 'alt-delete', command: '\\x1bd' },
+        { keystroke: 'cmd-backspace', command: '\\x1bw' },
+        { keystroke: 'cmd-delete', command: '\\x10B' }
+      ]
+
+      assert.deepEqual(schema.defaultValue('keybindings'), defaultKeybindings);
+    });
   });
 });
