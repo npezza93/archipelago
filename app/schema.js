7bc7cba72228bd8a26234e4e00e979598d5ce4d3
Nick Pezza <npezza93@gmail.com>
2018-08-30 22:18:45 -0400

    Fix lints

--- app/schema.js
+++ app/schema.js
@@ -1,102 +1,99 @@
-const CSON                  = require('season');
-const { join }              = require('path');
-const { getValueAtKeyPath } = require('key-path-helpers');
-const { splitKeyPath }      = require('key-path-helpers');
-const { pushKeyPath }       = require('key-path-helpers');
-const rootSchema            = require('./schema.json')
+const { splitKeyPath } = require('key-path-helpers')
+const { pushKeyPath }  = require('key-path-helpers')
+const rootSchema       = require('./schema.json')
 
 module.exports =
 class Schema {
   constructor() {
-    this.schema = { 'type': 'object', 'properties': rootSchema };
+    this.schema = { 'type': 'object', 'properties': rootSchema }
   }
 
   propertiesGroupedBySetting() {
-    const propertiesBySetting = {};
+    const propertiesBySetting = {}
 
-    const properties = this._childPropertiesFromSchema(this.schema);
+    const properties = this._childPropertiesFromSchema(this.schema)
     for (let property in properties) {
-      const { settings } = properties[property];
+      const { settings } = properties[property]
       if (propertiesBySetting[settings.title] == null) {
-        propertiesBySetting[settings.title] = [];
+        propertiesBySetting[settings.title] = []
       }
       propertiesBySetting[settings.title].push(
         { [property]: properties[property] }
-      );
+      )
     }
 
-    return propertiesBySetting;
+    return propertiesBySetting
   }
 
   defaultValue(keyPath) {
-    const childSchema = this.getSchema(keyPath);
-    let value;
+    const childSchema = this.getSchema(keyPath)
+    let value
 
     if (childSchema.type === 'object') {
-      value = this._objectDefaultValue(keyPath, childSchema);
+      value = this._objectDefaultValue(keyPath, childSchema)
     } else {
-      value = this._topLevelDefaultValue(childSchema);
+      value = this._topLevelDefaultValue(childSchema)
     }
 
-    return value;
+    return value
   }
 
   getSchema(keyPath) {
-    const keys = splitKeyPath(keyPath);
-    let { schema } = this;
+    const keys = splitKeyPath(keyPath)
+    let { schema } = this
 
     for (let key of keys) {
-      let childSchema;
+      let childSchema
       if (schema.type === 'object') {
-        childSchema = schema.properties[key];
+        childSchema = schema.properties[key]
       }
-      schema = childSchema;
+      schema = childSchema
     }
 
-    if (this.isEnabled(schema)) { return schema; }
+    if (this.isEnabled(schema)) { return schema }
   }
 
   isEnabled(schema) {
-    const defaultPlatforms = ['linux', 'win32', 'darwin'];
+    const defaultPlatforms = ['linux', 'win32', 'darwin']
 
-    return (schema.enabledOn || defaultPlatforms).includes(process.platform);
+    return (schema.enabledOn || defaultPlatforms).includes(process.platform)
   }
 
   // private
 
   _objectDefaultValue(keyPath, childSchema) {
-    const defaults = {};
+    const defaults = {}
 
     for (let key in (childSchema.properties || {})) {
-      defaults[key] = this.defaultValue(pushKeyPath(keyPath, key));
+      defaults[key] = this.defaultValue(pushKeyPath(keyPath, key))
     }
 
-    return defaults;
+    return defaults
   }
 
   _topLevelDefaultValue(childSchema) {
-    const { defaultValue, platformSpecific } = childSchema;
+    const { defaultValue, platformSpecific } = childSchema
 
     if (defaultValue != undefined && platformSpecific) {
-      return defaultValue[process.platform];
+      return defaultValue[process.platform]
     } else {
-      return defaultValue;
+      return defaultValue
     }
   }
 
   _childPropertiesFromSchema(schema, keyPath = null) {
     if (schema.type === 'object') {
-      const properties = {};
-      const object = schema.properties || {};
+      const properties = {}
+      const object = schema.properties || {}
       for (let childKeyPath in object) {
-        const childSchema = object[childKeyPath];
-        const propertyKeyPath = pushKeyPath(keyPath, childKeyPath);
-        const childProperties = this._childPropertiesFromSchema(childSchema, propertyKeyPath);
-        Object.assign(properties, childProperties);
+        const childSchema = object[childKeyPath]
+        const propertyKeyPath = pushKeyPath(keyPath, childKeyPath)
+        const childProperties = this._childPropertiesFromSchema(childSchema, propertyKeyPath)
+        Object.assign(properties, childProperties)
       }
-      return properties;
+      return properties
     } else if (this.isEnabled(schema)) {
-      return { [keyPath]: schema };
+      return { [keyPath]: schema }
     }
   }
-};
+}
