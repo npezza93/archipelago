ee58b3490a4f9d1f9fef3d61fd2d7b164eff0fad
Nick Pezza <npezza93@gmail.com>
2018-09-04 19:07:57 -0400

    Fix global lints

--- app/global.js
+++ app/global.js
@@ -1,9 +1,9 @@
 const ConfigFile = require('./config-file')
 const ProfileManager = require('./profile_manager')
-const Config         = require('./config')
+const Config = require('./config')
 
 module.exports = {
-  configFile: new ConfigFile,
-  config: new Config,
-  profileManager: new ProfileManager(new ConfigFile)
+  configFile: new ConfigFile(),
+  config: new Config(),
+  profileManager: new ProfileManager(new ConfigFile())
 }
