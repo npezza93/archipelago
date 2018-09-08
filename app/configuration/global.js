c2947107da51f3ee65e3d14e8fe8b3965b27b1a7
Nick Pezza <npezza93@gmail.com>
2018-09-08 18:07:19 -0400

    Validate active profile when requiring the global config

--- app/configuration/global.js
+++ app/configuration/global.js
@@ -2,8 +2,12 @@ const ConfigFile = require('./config-file')
 const ProfileManager = require('./profile-manager')
 const Config = require('./config')
 
-module.exports = {
+const globalConfig = {
   configFile: new ConfigFile(),
   config: new Config(),
   profileManager: new ProfileManager(new ConfigFile())
 }
+
+globalConfig.profileManager.validate()
+
+module.exports = globalConfig
