c9fe54ee7378c73726cf0a2ab6b403ba71d64d91
Nick Pezza <npezza93@gmail.com>
2018-09-08 18:24:39 -0400

    Decaffeinate renderer index :coffee:

---
+++ app/renderer/index.js
@@ -0,0 +1,44 @@
+/* global document, archipelago */
+/* eslint guard-for-in: "off" */
+
+global.archipelago = require('../configuration/global')
+
+const {ipcRenderer} = require('electron')
+const React = require('react')
+const ReactDOM = require('react-dom')
+const App = require('./app')
+
+const styleProperties = {
+  fontFamily: '--font-family',
+  windowBackground: '--background-color',
+  tabColor: '--tab-color',
+  tabBorderColor: '--tab-border-color',
+  fontSize: '--font-size',
+  padding: '--terminal-padding',
+  'theme.selection': '--selection-color'
+}
+
+document.addEventListener('DOMContentLoaded', () => {
+  archipelago.app = ReactDOM.render(
+    React.createElement(App), document.getElementById('root')
+  )
+
+  for (const selector in styleProperties) {
+    const cssVar = styleProperties[selector]
+    const element = document.documentElement
+
+    element.style.setProperty(cssVar, archipelago.config.get(selector))
+  }
+})
+
+ipcRenderer.on('new-tab', () => {
+  if (!archipelago.config.get('singleTabMode')) {
+    return archipelago.app.addTab()
+  }
+})
+ipcRenderer.on('split-horizontal', () => archipelago.app.split('horizontal'))
+ipcRenderer.on('split-vertical', () => archipelago.app.split('vertical'))
+
+Object.entries(styleProperties).forEach(property =>
+  archipelago.config.onDidChange(property[0], newValue => document.documentElement.style.setProperty(property[1], newValue))
+)
--- app/renderer/init.js
+++
@@ -1,8 +0,0 @@
-require('coffeescript').register()
-KeymapManager = require('atom-keymap')
-
-global.archipelago = { ...require('../configuration/global'), keymaps: new KeymapManager }
-
-archipelago.keymaps.mappings = archipelago.config.get('keybindings')
-
-require('./renderer')
