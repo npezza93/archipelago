06ea230536905c32fe4bc2e3325929b24aeb4cab
Nick Pezza <npezza93@gmail.com>
2018-09-09 21:57:34 -0400

    Decaffeinate color field :coffee:

---
+++ app/settings/fields/color-field.js
@@ -0,0 +1,72 @@
+const {Component, createElement} = require('react')
+const {TextField} = require('rmwc')
+const {ChromePicker} = require('react-color')
+
+module.exports =
+class ColorField extends Component {
+  constructor(props) {
+    super(props)
+
+    this.state = {active: false}
+  }
+
+  render() {
+    return createElement(
+      'div', {
+        className: 'color-container',
+        key: this.props.datakey,
+        style: this.state.active ? {zIndex: 2} : undefined
+      },
+      this.backdrop(),
+      this.text()
+    )
+  }
+
+  backdrop() {
+    if (this.state.active) {
+      return createElement(
+        'div', {
+          style: {position: 'fixed', top: 0, left: 0, right: 0, bottom: 0},
+          onClick: () => this.setState({active: false})
+        }
+      )
+    }
+  }
+
+  text() {
+    return createElement(
+      TextField, {
+        datakey: this.props.datakey,
+        label: this.props.label,
+        value: this.props.value,
+        onClick: () => this.setState({active: true}),
+        onChange: () => {}
+      },
+      this.picker()
+    )
+  }
+
+  picker() {
+    if (this.state.active) {
+      return createElement(
+        'div',
+        {className: 'color-picker'},
+        createElement(
+          ChromePicker, {
+            color: this.props.value,
+            onChangeComplete: color => {
+              let rgba
+              if (color.rgb.a === 1) {
+                rgba = `rgb(${color.rgb.r},${color.rgb.g},${color.rgb.b})`
+              } else {
+                rgba = `rgba(${Object.values(color.rgb).join(',')})`
+              }
+
+              this.props.onChange.call(this, rgba)
+            }
+          }
+        )
+      )
+    }
+  }
+}
