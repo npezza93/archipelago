3c64686f296eb481ab1c1de13395534c8aee9470
Nick Pezza <npezza93@gmail.com>
2018-09-09 21:45:21 -0400

    Decaffeinate settings header :coffee:

---
+++ app/settings/header.js
@@ -0,0 +1,26 @@
+const {Component, createElement} = require('react')
+
+module.exports =
+class Header extends Component {
+  render() {
+    return createElement(
+      'archipelago-header',
+      {},
+      Object.keys(this.props.headings).map(heading => {
+        return this.renderHeading(heading)
+      })
+    )
+  }
+
+  createHeading(heading) {
+    const headingPosition = this.props.headings[heading]
+
+    return createElement(
+      'div', {
+        key: heading,
+        position: headingPosition
+      },
+      heading.titleize
+    )
+  }
+}
