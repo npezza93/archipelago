cbef5df88bf5a059d70ff1c9737db9ee0063f173
Nick Pezza <npezza93@gmail.com>
2018-10-07 22:31:17 -0400

    AllFields and Property js -> jsx

---
+++ app/renderer/settings/all-fields.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+/* eslint-disable import/no-unresolved */
+import coreFields from '@/settings/fields/core-fields'
+import ArrayField from '@/settings/fields/array-field'
+/* eslint-enable import/no-unresolved */
+
+const array = (property, value, schema, onChange) => {
+  return <ArrayField
+    property={property}
+    value={value}
+    schema={schema}
+    onChange={onChange.bind(this)} />
+}
+
+export default {array, ...coreFields}
---
+++ app/renderer/settings/property.jsx
@@ -0,0 +1,46 @@
+import React from 'react'
+import ipc from 'electron-better-ipc'
+
+/* eslint-disable import/no-unresolved */
+const allFields = require('@/settings/all-fields')
+/* eslint-enable import/no-unresolved */
+
+export default class Property extends React.Component {
+  constructor(props) {
+    super(props)
+
+    this.state = {
+      [props.property]: ipc.sendSync('get-preferences-sync', props.property)
+    }
+
+    this.bindListener()
+  }
+
+  render() {
+    return allFields[this.fieldType()].call(
+      this,
+      this.props.property,
+      this.state[this.props.property],
+      this.props.schema,
+      newValue => this.profileManager.set(this.props.property, newValue)
+    )
+  }
+
+  fieldType() {
+    let {type} = this.props.schema
+
+    if (type === 'string' && this.props.schema.color) {
+      type = 'color'
+    }
+
+    return type
+  }
+
+  bindListener() {
+    // this.profileManager.onDidChange(this.props.property, newValue => {
+    //   if (this.state[this.props.property] !== newValue) {
+    //     return this.setState({[this.props.property]: newValue})
+    //   }
+    // })
+  }
+}
