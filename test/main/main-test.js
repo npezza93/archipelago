d640cecbd3b01788ad10511c16b3dafacd431103
Nick Pezza <npezza93@gmail.com>
2018-11-23 14:39:27 -0500

    Test terminal splitting

--- test/main/main-test.js
+++ test/main/main-test.js
@@ -47,4 +47,45 @@ describe('Application launch', function () {
       assert.isEmpty(filteredLogs, 'Exception in renderer process encountered')
     })
   })
+
+  it('splits the terminal horizontally', () => {
+    let keys
+    if (process.platform === 'darwin') {
+      keys = ['cmd', 's']
+    } else {
+      keys = ['ctrl', 's']
+    }
+    assertElementCount(this.app, 'archipelago-terminal', 1, () => {
+      return this.app.client.keys(keys).then(() => {
+        assertElementCount(this.app, 'archipelago-terminal', 2)
+        assert(this.app.client.isExisting('.SplitPane.horizontal'))
+        assert.isFalse(this.app.client.isExisting('.SplitPane.vertical'))
+      })
+    })
+  })
+
+  it('splits the terminal vertically', () => {
+    let keys
+    if (process.platform === 'darwin') {
+      keys = ['cmd', 'shift', 's']
+    } else {
+      keys = ['ctrl', 'shift', 's']
+    }
+    assertElementCount(this.app, 'archipelago-terminal', 1, () => {
+      return this.app.client.keys(keys).then(() => {
+        assertElementCount(this.app, 'archipelago-terminal', 2)
+        assert(this.app.client.isExisting('.SplitPane.vertical'))
+        assert.isFalse(this.app.client.isExisting('.SplitPane.horizontal'))
+      })
+    })
+  })
 })
+
+function assertElementCount(app, selector, length, callback) {
+  app.client.elements(selector).then(elements => {
+    assert.equal(elements.length, length)
+    if (callback) {
+      callback()
+    }
+  })
+}
