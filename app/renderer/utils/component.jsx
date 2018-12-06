809814f4115131fd49da85ddced7f2367f2f7d37
Nick Pezza <npezza93@gmail.com>
2018-12-06 11:45:44 -0500

    Create new component to act as a base component to handle autoBinding and subs

---
+++ app/renderer/utils/component.jsx
@@ -0,0 +1,49 @@
+/* global window */
+
+import {darkMode} from 'electron-util'
+import React from 'react'
+import {CompositeDisposable} from 'event-kit'
+import autoBind from 'auto-bind'
+
+export default class Component extends React.Component {
+  constructor(props) {
+    super(props)
+    autoBind(this)
+
+    this.state = this.initialState()
+    this.subscriptions = new CompositeDisposable()
+    this.initialize()
+
+    window.addEventListener('beforeunload', this.cleanup)
+    this.bindListeners()
+  }
+
+  initialState() {
+    return {}
+  }
+
+  cleanup() {
+    this.subscriptions.dispose()
+  }
+
+  componentWillUnmount() {
+    this.cleanup()
+    window.removeEventListener('beforeunload', this.cleanup)
+  }
+
+  get theme() {
+    if (this.state.isDarkMode) {
+      return 'dark'
+    }
+
+    return 'light'
+  }
+
+  handleDarkModeChange() {
+    this.setState({isDarkMode: darkMode.isEnabled})
+  }
+
+  bindListeners() {}
+
+  initialize() {}
+}
