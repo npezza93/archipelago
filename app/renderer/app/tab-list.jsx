77a0a3fdd2add17e0112da9de681db079103e378
Nick Pezza <npezza93@gmail.com>
2018-12-10 18:53:11 -0500

    Animate tabs opening and closing

--- app/renderer/app/tab-list.jsx
+++ app/renderer/app/tab-list.jsx
@@ -1,20 +1,26 @@
 import React from 'react'
+import ReactCSSTransitionGroup from 'react-addons-css-transition-group'
 import Component from '../utils/component.jsx'
 import Tab from './tab.jsx'
 
 export default class TabList extends Component {
   render() {
     return <archipelago-tab-list>
-      {this.props.tabs.map(tab => (
-        <Tab
-          id={tab.id}
-          key={tab.id}
-          title={tab.title}
-          isUnread={tab.isUnread}
-          active={this.props.currentTabId === tab.id}
-          selectTab={this.props.selectTab}
-          removeTab={this.props.removeTab} />
-      ))}
+      <ReactCSSTransitionGroup
+        transitionName="archipelago-tab"
+        transitionEnterTimeout={300}
+        transitionLeaveTimeout={300}>
+        {this.props.tabs.map(tab => (
+          <Tab
+            id={tab.id}
+            key={tab.id}
+            title={tab.title}
+            isUnread={tab.isUnread}
+            active={this.props.currentTabId === tab.id}
+            selectTab={this.props.selectTab}
+            removeTab={this.props.removeTab} />
+        ))}
+      </ReactCSSTransitionGroup>
     </archipelago-tab-list>
   }
 }
