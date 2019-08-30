85fe1d329be858550199a82f53d0a0eca89b42f5
Nick Pezza <npezza93@gmail.com>
2019-08-29 20:34:11 -0400

    last cleanup

--- test/main/main-test.js
+++ test/main/main-test.js
@@ -61,60 +61,60 @@ describe('Application launch', function () {
     return assert.isFalse(await this.app.client.isExisting('.SplitPane.vertical'))
   })
 
-  // it('splits the terminal vertically', async () => {
-  //   const initalElements = await this.app.client.elements('archipelago-terminal')
-  //   assert.equal(initalElements.value.length, 1)
-  //   robot.keyTap('s', ['shift', cmdOrCtrl()])
-  //   await this.app.client.waitForVisible('.SplitPane.vertical')
-  //   const afterElements = await this.app.client.elements('archipelago-terminal')
-  //   assert.equal(afterElements.value.length, 2)
-  //   assert(await this.app.client.isExisting('.SplitPane.vertical'))
-  //   return assert.isFalse(await this.app.client.isExisting('.SplitPane.horizontal'))
-  // })
+  it('splits the terminal vertically', async () => {
+    const initalElements = await this.app.client.elements('archipelago-terminal')
+    assert.equal(initalElements.value.length, 1)
+    robot.keyTap('s', ['shift', cmdOrCtrl()])
+    await this.app.client.waitForVisible('.SplitPane.vertical')
+    const afterElements = await this.app.client.elements('archipelago-terminal')
+    assert.equal(afterElements.value.length, 2)
+    assert(await this.app.client.isExisting('.SplitPane.vertical'))
+    return assert.isFalse(await this.app.client.isExisting('.SplitPane.horizontal'))
+  })
 
-  // it('adds a new tab', async () => {
-  //   await setSingleTabMode(false, this.app)
-  //   const initalElements = await this.app.client.elements('archipelago-terminal')
-  //   assert.equal(initalElements.value.length, 1)
-  //   robot.keyTap('t', cmdOrCtrl())
-  //   const afterElements = await this.app.client.elements('archipelago-terminal')
-  //   assert.equal(afterElements.value.length, 2)
-  //   const tabElements = await this.app.client.elements('archipelago-tab')
-  //   return assert.equal(tabElements.value.length, 2)
-  // })
-  //
-  // it('doesnt add a new tab in single tab mode', async () => {
-  //   await setSingleTabMode(true, this.app)
-  //   const initalElements = await this.app.client.elements('archipelago-terminal')
-  //   assert.equal(initalElements.value.length, 1)
-  //   robot.keyTap('t', cmdOrCtrl())
-  //   const afterElements = await this.app.client.elements('archipelago-terminal')
-  //   assert.equal(afterElements.value.length, 1)
-  //   const tabElements = await this.app.client.elements('archipelago-tab')
-  //   return assert.equal(tabElements.value.length, 1)
-  // })
-  //
-  // describe('tab closures', () => {
-  //   it('closes a tab', async () => {
-  //     await setSingleTabMode(false, this.app)
-  //     robot.keyTap('t', cmdOrCtrl())
-  //     await this.app.client.pause(2000)
-  //     let tabElements = await this.app.client.elements('archipelago-tab')
-  //     assert.equal(tabElements.value.length, 2)
-  //     await this.app.client.click('archipelago-tab div')
-  //     await this.app.client.pause(2000)
-  //     tabElements = await this.app.client.elements('archipelago-tab')
-  //     assert.equal(tabElements.value.length, 1)
-  //   })
-  //
-  //   it('close button doesnt appear on last tab', async () => {
-  //     await setSingleTabMode(false, this.app)
-  //     const tabElements = await this.app.client.elements('archipelago-tab')
-  //     assert.equal(tabElements.value.length, 1)
-  //     const closeButton = await this.app.client.isVisible('archipelago-tab div')
-  //     assert.isFalse(closeButton)
-  //   })
-  // })
+  it('adds a new tab', async () => {
+    await setSingleTabMode(false, this.app)
+    const initalElements = await this.app.client.elements('archipelago-terminal')
+    assert.equal(initalElements.value.length, 1)
+    robot.keyTap('t', cmdOrCtrl())
+    const afterElements = await this.app.client.elements('archipelago-terminal')
+    assert.equal(afterElements.value.length, 2)
+    const tabElements = await this.app.client.elements('archipelago-tab')
+    return assert.equal(tabElements.value.length, 2)
+  })
+
+  it('doesnt add a new tab in single tab mode', async () => {
+    await setSingleTabMode(true, this.app)
+    const initalElements = await this.app.client.elements('archipelago-terminal')
+    assert.equal(initalElements.value.length, 1)
+    robot.keyTap('t', cmdOrCtrl())
+    const afterElements = await this.app.client.elements('archipelago-terminal')
+    assert.equal(afterElements.value.length, 1)
+    const tabElements = await this.app.client.elements('archipelago-tab')
+    return assert.equal(tabElements.value.length, 1)
+  })
+
+  describe('tab closures', () => {
+    it('closes a tab', async () => {
+      await setSingleTabMode(false, this.app)
+      robot.keyTap('t', cmdOrCtrl())
+      await this.app.client.pause(2000)
+      let tabElements = await this.app.client.elements('archipelago-tab')
+      assert.equal(tabElements.value.length, 2)
+      await this.app.client.click('archipelago-tab div')
+      await this.app.client.pause(2000)
+      tabElements = await this.app.client.elements('archipelago-tab')
+      assert.equal(tabElements.value.length, 1)
+    })
+
+    it('close button doesnt appear on last tab', async () => {
+      await setSingleTabMode(false, this.app)
+      const tabElements = await this.app.client.elements('archipelago-tab')
+      assert.equal(tabElements.value.length, 1)
+      const closeButton = await this.app.client.isVisible('archipelago-tab div')
+      assert.isFalse(closeButton)
+    })
+  })
 })
 
 async function setSingleTabMode(checked, app) {
