0ba4788b14b89fc85a39f8ec727c1428f203a6e6
Nick Pezza <pezza@hey.com>
2021-07-11 22:10:26 -0400

    Handle updating keybindings

--- app/renderer/settings/controllers/keybinding_controller.js
+++ app/renderer/settings/controllers/keybinding_controller.js
@@ -1,8 +1,10 @@
 import { Controller } from 'stimulus'
 import {ipcRenderer as ipc} from 'electron-better-ipc'
 
+import formatAccelerator from '../../utils/format-accelerator'
+
 export default class extends Controller {
-  static values = { keystroke: String, command: String }
+  static values = { keystroke: String, command: String, index: Number }
 
   select(event) {
     document.querySelectorAll('#keybindings article > div').forEach(row => {
@@ -12,11 +14,11 @@ export default class extends Controller {
   }
 
   edit(event) {
-    document.body.insertAdjacentHTML('beforeend', `<div class="backdrop" data-controller='keybinding-capturer'>
+    document.body.insertAdjacentHTML('beforeend', `<div class="backdrop" data-controller='keybinding-capturer' data-keybinding-capturer-index-value=${this.indexValue}>
       <div class='modal'>
         <div class='flex flex-col h-full w-full'>
           <div class='flex-1 flex flex-col w-full align-center gap-10 mt-15'>
-            <div data-keybinding-capturer-target='keystroke'>${this.keystrokeValue}</div>
+            <div id='key-recorder' class='w-60p' value='${this.keystrokeValue}' data-keybinding-capturer-target='keystroke' data-action='click->keybinding-capturer#startCapture'>${formatAccelerator(this.keystrokeValue)}</div>
             <input type="text" name="command" value="${this.commandValue}" autofocus class='w-60p' data-keybinding-capturer-target='command'>
           </div>
           <div class='footer flex flex-row justify-end'>
