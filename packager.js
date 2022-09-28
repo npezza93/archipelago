3205edea940fd55cda03c2438eb19a33f1f9f778
Nick Pezza <pezza@hey.com>
2022-09-28 16:27:40 -0400

    Manually sign

--- packager.js
+++ packager.js
@@ -1,6 +1,8 @@
 const packager = require('electron-packager')
 const {execSync} = require('child_process')
 const {rebuild} = require('electron-rebuild')
+const { sign } = require('@electron/osx-sign')
+const { notarize } = require('electron-notarize');
 
 packager({
   dir: process.cwd(),
@@ -14,22 +16,31 @@ packager({
     await rebuild({ buildPath, electronVersion, arch })
     callback()
   }],
-  osxSign: {
-    identity: 'Developer ID Application: Nick Pezza (4K4322K3MA)',
-    'hardened-runtime': true,
-    entitlements: `${process.cwd()}/entitlements.plist`,
-    'entitlements-inherit': `${process.cwd()}/entitlements.plist`,
-    'signature-flags': 'library'
-  },
-  osxNotarize: {
-    appleId: process.env["APPLE_ID"],
-    appleIdPassword: process.env["APPLE_PASSWORD"]
-  }
+  // osxSign: {
+  //   entitlements: `${process.cwd()}/entitlements.plist`,
+  //   'entitlements-inherit': `${process.cwd()}/entitlements.plist`,
+  //   'signature-flags': 'library'
+  // },
+  // osxNotarize: {
+  //   appleId: process.env["APPLE_ID"],
+  //   appleIdPassword: process.env["APPLE_PASSWORD"]
+  // }
 }).then((files) => {
     files.forEach((pathName) => {
+      console.log(`\nSigning ${pathName}`)
+
+      sign({
+        app: `${pathName}/Archipelago.app`,
+        hardenedRuntime: true,
+        identity: 'Developer ID Application: Nick Pezza (4K4322K3MA)',
+        entitlements: `${process.cwd()}/entitlements.plist`
+      })
+
+      console.log(`Signed ${pathName}`)
+
       const file = pathName.split("/").pop()
-      console.log(`zipping ${pathName}`)
+      console.log(`Zipping ${pathName}`)
       const cmd = `cd ${pathName} && rm -f ${file}.zip && zip -r --symlinks ${file}.zip Archipelago.app && mv ${file}.zip ../ && cd -`
-      // execSync(cmd, {stdio: 'inherit'})
+      execSync(cmd, {stdio: 'inherit'})
     })
   })
