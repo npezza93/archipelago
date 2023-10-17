0922fb7c061189fae4a1635e26385f8f382a7621
Nick Pezza <pezza@hey.com>
2023-10-16 21:09:58 -0400

    Initial Commit

---
+++ archipelago2/AppDelegate.swift
@@ -0,0 +1,29 @@
+//
+//  AppDelegate.swift
+//  archipelago2
+//
+//  Created by Nick Pezza on 10/16/23.
+//
+
+import Cocoa
+
+@main
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+    func applicationSupportsSecureRestorableState(_ app: NSApplication) -> Bool {
+        return true
+    }
+
+
+}
+
---
+++ archipelago2/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  archipelago2
+//
+//  Created by Nick Pezza on 10/16/23.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
